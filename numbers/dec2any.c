1c6e4897e20cca0f8c713f6024da2419aefe7f00
Dipanjan Patra <patraanjan23@gmail.com>
2019-11-26 20:56:42 +0530

    reformatted code

--- numbers/dec2any.c
+++ numbers/dec2any.c
@@ -8,71 +8,64 @@ char syms[MAX] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 void append(char *, char);
 char *strRev(char *);
 
-int main(int argc, char *argv[])
-{
-    long pbase = 10;
-    long dec, tmp, base, r;
-    char any[SZ] = "";
-    
-    if (argc != 2)
-    {
-        printf("usage: ./dec2any <base>\n");
-        return 0;
-    }
-    
-    base = atoi(argv[1]);
-    if (base < 2 || base > 36)
-    {
-        printf("base must be between [2, 36]\n");
-        return 0;
-    }
+int main(int argc, char *argv[]) {
+  long pbase = 10;
+  long dec, tmp, base, r;
+  char any[SZ] = "";
 
-    printf("enter a decimal number: ");
-    scanf("%ld", &dec);
-
-    tmp = dec;
-    
-    while (tmp != 0)
-    {
-        r = tmp % base;
-        tmp /= base;
-        
-        append(any, syms[r]);
-    }
-    printf("(%ld)%ld = (%s)%ld\n", dec, pbase, strRev(any), base);
-
+  if (argc != 2) {
+    printf("usage: ./dec2any <base>\n");
     return 0;
+  }
+
+  base = atoi(argv[1]);
+  if (base < 2 || base > 36) {
+    printf("base must be between [2, 36]\n");
+    return 0;
+  }
+
+  printf("enter a decimal number: ");
+  scanf("%ld", &dec);
+
+  tmp = dec;
+
+  while (tmp != 0) {
+    r = tmp % base;
+    tmp /= base;
+
+    append(any, syms[r]);
+  }
+  printf("(%ld)%ld = (%s)%ld\n", dec, pbase, strRev(any), base);
+
+  return 0;
 }
 
-void append(char *str, char c)
-{
-    int len;
-    for (len = 0; str[len] != '\0'; len++);
-    if (len + 2 < SZ)
-    {
-        str[len] = c;
-        str[len+1] = '\0';
-    }
-    else return;
+void append(char *str, char c) {
+  int len;
+  for (len = 0; str[len] != '\0'; len++)
+    ;
+  if (len + 2 < SZ) {
+    str[len] = c;
+    str[len + 1] = '\0';
+  } else
+    return;
 }
 
-char *strRev(char *s)
-{
-    int l, i;
-    for (l = 0; s[l] != '\0'; l++)
-        ;
-    for (i = 0; i < l / 2; i++)
-    {
-        char c = s[i];
-        s[i] = s[l - i - 1];
-        s[l - i - 1] = c;
-    }
-    return s;
+char *strRev(char *s) {
+  int l, i;
+  for (l = 0; s[l] != '\0'; l++)
+    ;
+  for (i = 0; i < l / 2; i++) {
+    char c = s[i];
+    s[i] = s[l - i - 1];
+    s[l - i - 1] = c;
+  }
+  return s;
 }
 
 /* OUTPUT
 -------------------------------
-▶ ./dec2any.out 2 
+▶ ./dec2any.out 2
 enter a decimal number: 21
 (21)10 = (10101)2
 -------------------------------
